42ca208284a8b7a6710873f72d84fc0b1e79187e
AL-Khalil-Developers <alkhalildevelopers@gmail.com>
2019-10-20 07:09:35 +0500

    Initial commit

---
+++ app/src/main/java/com/alkhalildevelopers/bgvidapp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.alkhalildevelopers.bgvidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        videoView = findViewById(R.id.bg_vid_id);
+        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.bg_vid);
+        videoView.setVideoURI(uri);
+        videoView.start();
+
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setLooping(true);
+            }
+        });
+
+    }
+
+
+}
